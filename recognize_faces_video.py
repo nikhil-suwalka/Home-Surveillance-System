7534f647834bac89f44d3fcd3ec3482ecee57399
Nishit Shanbhag <nishit.shanbhag@gmail.com>
2020-03-11 18:44:05 +0530

    improved performance using resize

--- recognize_faces_video.py
+++ recognize_faces_video.py
@@ -3,29 +3,29 @@
 # python recognize_faces_video.py --encodings encodings.pickle --output output/jurassic_park_trailer_output.avi --display 0
 
 # import the necessary packages
+import concurrent.futures
 import os
-import shutil
 from PIL import Image
-import time
 from imutils.video import VideoStream
 import face_recognition
-import argparse
-import imutils
 import pickle
 from datetime import datetime
 import cv2
 
-# construct the argument parser and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-e", "--encodings", required=True,
-# 	help="path to serialized db of facial encodings")
-# ap.add_argument("-o", "--output", type=str,
-# 	help="path to output video")
-# ap.add_argument("-y", "--display", type=int, default=1,
-# 	help="whether or not to display output frame to screen")
-# ap.add_argument("-d", "--detection-method", type=str, default="cnn",
-# 	help="face detection model to use: either `hog` or `cnn`")
-# args = vars(ap.parse_args())
+
+# def imageSavefromFrame(rgb, date_time):
+def imageSavefromFrame(arr):
+    # print("Task 1 assigned to thread: {}".format(threading.current_thread().name))
+    print("ID of process running task 1: {}".format(os.getpid()))
+    cv2.imwrite(arr[1], arr[0])
+    image = Image.open(arr[1])
+    # .rotate(270, expand=True)
+    image.thumbnail((600, 468), Image.ANTIALIAS)
+    image.save(arr[1], quality=40, optimize=True)
+    print(arr[1])
+    return arr[1]
+
+
 args = {'encodings': 'encodings.pickle', 'output': None, 'display': 1, 'detection_method': 'cnn'}
 
 # load the known faces and embeddings
@@ -36,15 +36,15 @@ data = pickle.loads(open(args["encodings"], "rb").read())
 # allow the camera sensor to warm up
 print("[INFO] starting video stream...")
 vs = VideoStream(src=1).start()
-writer = None
-time.sleep(2.0)
 
 found = False
 unknownFaces = 0
 knownFaces = 0
 counter = 0
-
+p = None
 # loop over frames from the video file stream
+
+images_to_save = []
 while True:
     # grab the frame from the threaded video stream
     frame = vs.read()
@@ -52,12 +52,14 @@ while True:
     # convert the input frame from BGR to RGB then resize it to have
     # a width of 750px (to speedup processing)
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    rgb = imutils.resize(frame, width=750)
+
+    rgb = cv2.resize(frame, (0, 0), fx=0.7, fy=0.7)
     r = frame.shape[1] / float(rgb.shape[1])
 
     # detect the (x, y)-coordinates of the bounding boxes
     # corresponding to each face in the input frame, then compute
     # the facial embeddings for each face
+
     boxes = face_recognition.face_locations(rgb, model=args["detection_method"])
     encodings = face_recognition.face_encodings(rgb, boxes)
     names = []
@@ -90,7 +92,7 @@ while True:
             # determine the recognized face with the largest number
             # of votes (note: in the event of an unlikely tie Python
             # will select first entry in the dictionary)
-            threshold = round(data["names"].count(name) * 0.60)
+            threshold = round(data["names"].count(name) * 0.70)
             if (counts[max(counts, key=counts.get)] >= threshold):
                 name = max(counts, key=counts.get)
             else:
@@ -101,25 +103,27 @@ while True:
                 knownFaces += 1
                 counter = 0
             now = datetime.now()
-            if (now.second % 3 == 0):
-                date_time = "temp\\" + now.strftime("%d-%m-%Y_%H-%M-%S") + ".png"
-                cv2.imwrite(date_time, rgb)
-                image = Image.open(date_time).rotate(270, expand=True)
-                image.thumbnail((600, 468), Image.ANTIALIAS)
-                image.save(date_time, quality=40, optimize=True)
+            # # TODO solve critical section problem
 
-        # update the list of names
+            if (now.second % 5 == 0):
+                date_time = "temp\\" + now.strftime("%d-%m-%Y_%H-%M-%S.%f") + ".png"
+                images_to_save.append([rgb, date_time])
+
         names.append(name)
 
     if (not found):
         counter += 1
-        if (counter > 10):
+        if (counter > 100):
             if (unknownFaces + knownFaces > 0 and (unknownFaces / (unknownFaces + knownFaces)) * 100 > 80):
                 print("Detected unknown person!")
+            if __name__ == '__main__':
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    results = executor.map(imageSavefromFrame, images_to_save)
             unknownFaces = 0
             knownFaces = 0
-            shutil.rmtree("temp")
-            os.mkdir("temp")
+            # shutil.rmtree("temp")
+            # os.mkdir("temp")
+            images_to_save.clear()
 
     print(unknownFaces, knownFaces)
 
@@ -138,32 +142,13 @@ while True:
         cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
                     0.75, (0, 255, 0), 2)
 
-    # if the video writer is None *AND* we are supposed to write
-    # the output video to disk initialize the writer
-    if writer is None and args["output"] is not None:
-        fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-        writer = cv2.VideoWriter(args["output"], fourcc, 20,
-                                 (frame.shape[1], frame.shape[0]), True)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
 
-    # if the writer is not None, write the frame with recognized
-    # faces t odisk
-    if writer is not None:
-        writer.write(frame)
-
-    # check to see if we are supposed to display the output frame to
-    # the screen
-    if args["display"] > 0:
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
-
-        # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-            break
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
-
-# check to see if the video writer point needs to be released
-if writer is not None:
-    writer.release()
